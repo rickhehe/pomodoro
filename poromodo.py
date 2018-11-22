63950df49c560820fb0b469bd25093d5bf8e2241
rick <i@rickhehe.com>
2018-11-22 21:48:15 +1300

    3 modes. What's next?

--- poromodo.py
+++ poromodo.py
@@ -1,41 +1,23 @@
-'''
-This is pretty much only a 25min timer.
-start
-get current time, plus 25 min, then make some noise
-use input to get next action, short break or long break
-'''
-
-from datetime import datetime
 import time
-from datetime import date
-from datetime import timedelta
-
-def work():
-    start_time = datetime.now()
-    break_time = start_time + timedelta(seconds = 1*60)
-    print(break_time)
-
-    while 1:
-        time.sleep(10.0) #1 min later
-
-        current_time = datetime.now()
-        time_left = break_time - current_time
-
-        if current_time < break_time:
-            print(time_left)
-
-        else:
-            print('take a break')
-            break
-
-#    print(current_time)
-
-def short_break():
-    print('take a break')
-    pass
-
-def long_break():
-    pass
+from datetime import timedelta, datetime
 
 
-work()
+def pomodoro(mode, scale):
+
+    start_time = datetime.now() 
+    duration = timedelta(seconds = standard_time[mode] * 60 * scale)
+    end_time = start_time + duration
+
+    print(i.replace('_', ' '), 'starts at:', start_time)
+    print(i.replace('_', ' '), 'ends at:', end_time)
+
+    time.sleep(duration.seconds)
+    print(datetime.now(), ', ', message[mode])
+
+#Set parameters.
+standard_time = {'work':25, 'short_break':5, 'long_break':15}
+message = {'work': 'take a break', 'short_break': 'go back to work', 'long_break': 'go back to work'}
+
+#basic test
+for i in ['work', 'short_break', 'long_break']:
+    pomodoro(i, scale = 0.1)
