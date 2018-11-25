6a580a24ce3c9a587e4d07d2c62c474c24055668
rick <i@rickhehe.com>
2018-11-25 15:26:12 +1300

    added strftime() to make the time look more beautiful.

--- pomodoro.py
+++ pomodoro.py
@@ -1,23 +1,29 @@
 import time
 from datetime import timedelta, datetime
 
-
-def pomodoro(mode, scale):
-
+#Scale is to reduce waiting time when testing. Set it to 1 unless testing.
+def pomodoro(mode, scale = 0.05):
     start_time = datetime.now() 
     duration = timedelta(seconds = standard_time[mode] * 60 * scale)
     end_time = start_time + duration
 
-    print(i.replace('_', ' '), 'starts at:', start_time)
-    print(i.replace('_', ' '), 'ends at:', end_time)
+    print(abbr[mode], 'starts at:', start_time.strftime('%X'))
+    print(abbr[mode], 'ends at:', end_time.strftime('%X'))
 
     time.sleep(duration.seconds)
-    print(datetime.now(), ', ', message[mode])
+    print(datetime.now(), ', ', end_message[mode])
 
 #Set parameters.
-standard_time = {'work':25, 'short_break':5, 'long_break':15}
-message = {'work': 'take a break', 'short_break': 'go back to work', 'long_break': 'go back to work'}
+standard_time = {'w':25, 's':5, 'l':15}
+end_message = {'w': 'take a break', 's': 'go back to work', 'l': 'go back to work'}
+abbr = {'w':'work', 's':'short_break', 'l':'long_break'}
 
-#basic test
-for i in ['work', 'short_break', 'long_break']:
-    pomodoro(i, scale = 0.1)
+while 1:
+    wsl = input('what are you going to do? w for work, s for short_break, l for long_break: ').lower()
+
+    if wsl.startswith('w') or wsl.startswith('s') or wsl.startswith('l'):
+        pomodoro(wsl[:1])
+    elif wsl.startswith('q'):
+        break
+    else:
+        print('typo? q for quit')
